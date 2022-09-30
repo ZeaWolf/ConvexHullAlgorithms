8f7ca6b49957814c42cb4336a7d5dfabd7e1cb5f
yongshn <yongshn220@gmail.com>
2022-09-30 10:37:12 +0900

    Quickhull on

--- func.cpp
+++ func.cpp
@@ -10,12 +10,13 @@ bool Func::DoPointConvex(D2D1_POINT_2F target, std::vector<D2D1_POINT_2F>* point
 void Func::DoQuickhull(std::vector<D2D1_POINT_2F>* points, std::vector<D2D1_POINT_2F>* results)
 {
     QuickHull* qh = new QuickHull(points, results);
+    qh->Calculate();
 }
 
 void Func::DoMinkowskiSum(std::vector<D2D1_POINT_2F>* arr1, std::vector<D2D1_POINT_2F>* arr2, std::vector<D2D1_POINT_2F>* results)
 {
     Minkowski* mk = new Minkowski();
-    return mk->CalculateSum(*arr1, *arr2, results);
+    mk->CalculateSum(*arr1, *arr2, results);
 }
 
 void Func::DoMinkowskiDiff(std::vector<D2D1_POINT_2F>* arr1, std::vector<D2D1_POINT_2F>* arr2, std::vector<D2D1_POINT_2F>* results)
